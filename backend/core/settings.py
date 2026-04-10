7ed19259c12f10d65f6df12db1e3e6a1211a2d3a
XantSpace <heis.buike99@gmail.com>
2026-04-10 18:52:05 +0100

    Migrating to postgreSQL

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -89,11 +89,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL', f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
